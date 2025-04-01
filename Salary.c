f9101a14d1e040e8aebbac1a6f519458cc1c5cb0
SubhamKumar-20 <smeesala@student.gitam.edu>
2025-04-01 13:12:55 +0530

    Create Salary.c

---
+++ Salary.c
@@ -0,0 +1,43 @@
+Write a C program to input basic salary of an employee and 
+        calculate its Gross salary according to following:
+        Basic Salary <= 10000 : HRA = 20%, DA = 80%
+        Basic Salary <= 20000 : HRA = 25%, DA = 90%
+       Basic Salary > 20000 : HRA = 30%, DA = 95%
+CODE :- 
+#include <stdio.h>
+             int main()
+ {
+    float basicSalary, hra, da, grossSalary;
+    printf("Enter the basic salary: ");
+    scanf("%f", &basicSalary);
+
+    // Calculate HRA and DA
+    if (basicSalary <= 10000)
+ {
+        hra = basicSalary * 0.20;
+        da = basicSalary * 0.80;
+  }
+ else if (basicSalary <= 20000)
+ {
+        hra = basicSalary * 0.25;
+        da = basicSalary * 0.90;
+    }
+ else {
+        hra = basicSalary * 0.30;
+        da = basicSalary * 0.95;
+         }
+  // Calculate gross salary
+    grossSalary = basicSalary + hra + da;
+   printf("Basic Salary: %.2f\n", basicSalary);
+    printf("HRA: %.2f\n", hra);
+    printf("DA: %.2f\n", da);
+    printf("Gross Salary: %.2f\n", grossSalary);
+   return 0;
+}
+
+OUTPUT:-
+Enter the basic salary: 8000
+Basic Salary: 8000.00
+HRA: 1600.00
+DA: 6400.00
+Gross Salary: 16000.00
