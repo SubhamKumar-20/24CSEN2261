ccef34867e4bdc36b85b329bbc7a311906586395
SubhamKumar-20 <smeesala@student.gitam.edu>
2025-04-01 13:29:55 +0530

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,22 @@
+try:
+    n = int(input("Enter the number of n for the Fibonacci series: "))
+    if n <= 0:
+        print("Number of n must be a positive integer.") 
+    elif n == 1:
+        print(0)
+    elif n == 2:
+        print(0, 1)
+    else:
+        series = [0, 1]
+        for i in range(2, n):
+            series.append(series[-1] + series[-2])
+            
+    print(f"The Fibonacci series with {n} n is: {series}")
+except ValueError:
+    print("Please enter a valid integer.")
+
+INPUT:-
+Enter the number of n for the Fibonacci series: 5
+
+OUTPUT:-
+The Fibonacci series with 5 terms is: [0, 1, 1, 2, 3]
